f3154b25db25edc23ddbb0e07f8a21203e60f79c
ShopMarket Dev <dev@shopmarket.com>
2026-01-24 10:21:25 -0300

    fix(auth): redireciona para home após login

--- shopmarket/settings.py
+++ shopmarket/settings.py
@@ -145,3 +145,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Auth redirects
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
