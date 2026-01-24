5dfe902ecebbc4a9368cf573f1088e78c458f1cc
ShopMarket Dev <dev@shopmarket.com>
2026-01-24 14:22:57 -0300

    fix(cart): implementa CSRF robusto via cookie para requisições AJAX

--- shopmarket/settings.py
+++ shopmarket/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.csrf',
                 'cart.context_processors.cart',
                 'stores.context_processors.shop_config',
                 'catalog.context_processors.categories_processor',
